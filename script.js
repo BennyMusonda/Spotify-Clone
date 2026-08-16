ecdd35aae8c8b8ed89a7e5e363b6d5747bc79692
Benny Musonda <92533370+BennyMusonda@users.noreply.github.com>
2026-08-16 23:30:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-//CALL OUT ELEMENTS BY THEiR ID's
-const audio = document.getElementById('nastyc');
-const playBtn = document.getElementById('playBtn');
-
-playBtn.addEventListener('click', () => {
-    playBtn.classList.toggle('is-playing');
-    
-    //EVENT LISTENER FOR THE BUTTONS
-    if (audio.paused){
-        audio.play();
-        playBtn.textContent = 'Pause';
-    }
-    else{
-        audio.pause();
-        playBtn.textContent = 'Play';
-    }
-})
 
