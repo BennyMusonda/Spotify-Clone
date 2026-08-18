5f3db6ddc45824ef6e095ab538efcb2e24a3b872
Benny Musonda <92533370+BennyMusonda@users.noreply.github.com>
2026-08-18 18:48:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+//GETTING REFERENCES FROM HTML ELEMENTS
+const audio = document.getElementById('audio_1');
+const playBtn = document.getElementById('playBtn');
+const playIcon = document.getElementById('playIcon');
 
+// GRAB AUDIO FILE FROM HTML
+const audio_1 = document.getElementById('audio1');
+
+//ADDING CLICKING EVENT OF THE SONG
+playBtn.addEventListener('click', () => {
+    if (audio.paused) {
+        audio.play();
+
+//SWAP ICON FROM PLAY TO PAUSE
+        playIcon.classList.remove('fa-circle-play');
+        playIcon.classList.add('fa-circle-pause');
+    } else {
+        audio.pause();
+        
+//SWAP ICON FROM PAUSE TO PLAY
+        playIcon.classList.remove('fa-circle-pause');
+        playIcon.classList.add('fa-circle-play');
+    }
+});
